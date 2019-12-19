9caad62038bc606d574e4aa49ff5fa766a1ef779
Max Safin <a4ron5@yandex.ru>
2019-12-19 19:18:25 +0500

    @lib add theme

---
+++ src/lib/theme/index.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-use-before-define */
+
+import { createEvent, createStore } from "effector"
+
+const themes = ["light", "dark"]
+
+const nextTheme = {
+  light: "light",
+  dark: "dark",
+}
+
+export const themeToggled = createEvent()
+
+export const $selectedTheme = createStore(activateTheme())
+
+$selectedTheme.on(themeToggled, (_, theme) => nextTheme[theme])
+
+$selectedTheme.watch(saveTheme)
+
+const activateTheme = () => {
+  const theme = localStorage.getItem("theme")
+  const defaultTheme = themes[0]
+
+  for (let i = 0; i < themes.length; i += 1) {
+    if (themes[i] === theme) {
+      return themes[i]
+    }
+  }
+  return defaultTheme
+}
+
+const saveTheme = (theme) => {
+  localStorage.setItem("theme", theme)
+}
