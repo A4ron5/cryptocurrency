5af027b0e359e176b85da5e8d924bce91d2be66e
Max Safin <a4ron5@yandex.ru>
2019-12-20 15:38:15 +0500

    @ui add dark and light themes

---
+++ src/ui/themes/dark.js
@@ -0,0 +1,8 @@
+import { css } from "styled-components"
+
+export const dark = css`
+  --surface: #292929;
+  --primary: #121212;
+  --primary-text: #fff;
+  --primary-border: #121212;
+`
---
+++ src/ui/themes/light.js
@@ -0,0 +1,8 @@
+import { css } from "styled-components"
+
+export const light = css`
+  --surface: #e5e5e5;
+  --primary: #fff;
+  --primary-text: #292929;
+  --primary-border: #dedede;
+`
