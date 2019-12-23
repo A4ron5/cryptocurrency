1c6d2c3e76c3fd48b4eaec9d6d1ccbe9f8e64746
Max Safin <a4ron5@yandex.ru>
2019-12-23 18:30:05 +0500

    add homepage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import * as React from "react"
-import { ThemeToggler } from "@features/themetoggler"
 import { Normalize } from "styled-normalize"
 import { ThemeProvider } from "@features/themeprovider"
-import { GlobalStyles } from "./ui/globalstyles"
+import { GlobalStyles } from "@ui/globalstyles"
+import { HomePage } from "./pages/home"
 
 export const App = () => {
   return (
     <ThemeProvider>
       <Normalize />
       <GlobalStyles />
-      <ThemeToggler />
+      <HomePage />
     </ThemeProvider>
   )
 }
