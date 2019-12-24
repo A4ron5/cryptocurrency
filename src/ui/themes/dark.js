7ecd79426c3f5f1a2cc9565ab4df05f64b7f5a91
Max Safin <a4ron5@yandex.ru>
2019-12-24 17:21:58 +0500

    ui(themes): added inverse colour, changed primary-text colour

--- src/ui/themes/dark.js
+++ src/ui/themes/dark.js
@@ -3,6 +3,8 @@ import { css } from "styled-components"
 export const dark = css`
   --surface: #292929;
   --primary: #121212;
-  --primary-text: #fff;
+  --primary-text: #740af5;
+  --primary-second-text: #f5510a;
+  --inverse: #fff;
   --primary-border: #121212;
 `
--- src/ui/themes/light.js
+++ src/ui/themes/light.js
@@ -3,6 +3,7 @@ import { css } from "styled-components"
 export const light = css`
   --surface: #e5e5e5;
   --primary: #fff;
-  --primary-text: #292929;
+  --primary-text: #f50a50;
+  --inverse: #121212;
   --primary-border: #dedede;
 `
