0d6ca7a2f977cfd4ea65c00da7a9b3ddeda6b608
Max Safin <a4ron5@yandex.ru>
2019-12-25 16:04:46 +0500

    ui(themes): changed colours

--- src/ui/themes/dark.js
+++ src/ui/themes/dark.js
@@ -3,8 +3,6 @@ import { css } from "styled-components"
 export const dark = css`
   --surface: #292929;
   --primary: #121212;
-  --primary-text: #740af5;
-  --primary-second-text: #f5510a;
-  --inverse: #fff;
+  --primary-text: #fff;
   --primary-border: #121212;
 `
--- src/ui/themes/light.js
+++ src/ui/themes/light.js
@@ -3,7 +3,6 @@ import { css } from "styled-components"
 export const light = css`
   --surface: #e5e5e5;
   --primary: #fff;
-  --primary-text: #f50a50;
-  --inverse: #121212;
+  --primary-text: #121212;
   --primary-border: #dedede;
 `
