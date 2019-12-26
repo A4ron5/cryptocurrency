f02214d5baec94a2ef10c82fa8d4fdd6eb47e1e2
Max Safin <a4ron5@yandex.ru>
2019-12-26 13:35:54 +0500

    ui(atoms/message): added

---
+++ src/ui/atoms/message.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const Message = styled.div`
+  display: block;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 40px;
+  color: var(--primary-text);
+`
