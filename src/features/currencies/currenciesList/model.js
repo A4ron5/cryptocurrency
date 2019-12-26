c972d70774176ff12cfcd046759a0121bba6c66f
Max Safin <a4ron5@yandex.ru>
2019-12-26 13:43:06 +0500

    features(currencies/currenciesList): added exception catch logic

--- src/features/currencies/currenciesList/model.js
+++ src/features/currencies/currenciesList/model.js
@@ -6,9 +6,17 @@ export const fetchedCurrencies = createEffect()
 
 fetchedCurrencies.use(request)
 
-export const $currencies = createStore([])
-  .on(fetchedCurrencies.done, (_, { result }) => result.data.splice(0, 100))
-  .on(fetchedCurrencies.fail, () => "something goes wrong")
+export const $currencies = createStore([]).on(
+  fetchedCurrencies.done,
+  (_, { result }) => result.data.splice(0, 100),
+)
+
+export const $currenciesError = createStore("").on(
+  fetchedCurrencies.fail,
+  () => "Something goes wrong :(",
+)
+
+fetchedCurrencies.fail.watch(console.log)
 
 export const loading = (loader) =>
   createComponent(fetchedCurrencies.pending, (_, pending) => pending && loader)
