3d616302deb1af185b6d7b67c98cb9e75d278218
Max Safin <a4ron5@yandex.ru>
2019-12-27 14:40:05 +0500

    app: added browser-router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import * as React from "react"
 import { Normalize } from "styled-normalize"
 import { ThemeProvider } from "@features/themeprovider"
 import { GlobalStyles } from "@ui/globalstyles"
-import { HomePage } from "./pages/home"
+import { BrowserRouter, Redirect } from "react-router-dom"
+import { Routes } from "./routes"
 
 export const App = () => {
   return (
     <ThemeProvider>
       <Normalize />
       <GlobalStyles />
-      <HomePage />
+      <BrowserRouter>
+        <Redirect to="/currencies" />
+        <Routes />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
