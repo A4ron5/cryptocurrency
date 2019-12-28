eae88fe6b77acd67a1d0c404b1f8e324056deeb1
Max Safin <a4ron5@yandex.ru>
2019-12-28 12:22:00 +0500

    pages(index): added /about and /featured to routes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,23 @@
 import { HomePage } from "./home/page"
 import { NotFoundPage } from "./notfound/page"
+import { AboutPage } from "./about/page"
+import { FeaturedPage } from "./featured/page"
 
 export const routes = () => [
   {
     path: "/currencies",
     exact: true,
     component: HomePage,
+  },
+  {
+    path: "/about",
+    exact: true,
+    component: AboutPage,
+  },
+  {
+    path: "/featured",
+    exact: true,
+    component: FeaturedPage,
   },
   {
     component: NotFoundPage,
