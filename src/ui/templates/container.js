57ff5bdbc30045c35b52d0c0b20756e4813cab27
Max Safin <a4ron5@yandex.ru>
2019-12-28 12:28:22 +0500

    ui(templates/conttainer): added media queries

--- src/ui/templates/container.js
+++ src/ui/templates/container.js
@@ -3,7 +3,10 @@ import styled from "styled-components"
 export const Container = styled.div`
   width: 1280px;
   margin: 0 auto;
-  @media (max-width: 1300px) {
+  @media (max-width: 1340px) {
     max-width: 768px;
+  }
+  @media (max-width: 870px) {
+    max-width: 540px;
   }
 `
