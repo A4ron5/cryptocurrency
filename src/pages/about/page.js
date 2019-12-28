68c51b2351c2718b718399dd7611194514521590
Max Safin <a4ron5@yandex.ru>
2019-12-28 12:19:02 +0500

    pages(about): added

---
+++ src/pages/about/page.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { CommonTemplate } from "@features/common/index"
+import { Container } from "@ui/templates"
+
+export const AboutPage = () => (
+  <CommonTemplate>
+    <Container>
+      <div>About</div>
+    </Container>
+  </CommonTemplate>
+)
