55d77feb8aab4843a4e2062ce4ba6cf913ba0b98
Max Safin <a4ron5@yandex.ru>
2019-12-28 12:22:30 +0500

    pages(home): add media queries

--- src/pages/home/page.js
+++ src/pages/home/page.js
@@ -1,15 +1,18 @@
 import React from "react"
 import styled from "styled-components"
-import { CommonCurrenciesTemplate } from "@features/common"
+import { CommonTemplate } from "@features/common"
 import { CurrenciesList } from "@features/currencies"
+import { Container } from "@ui/templates"
 
 export const HomePage = () => {
   return (
-    <CommonCurrenciesTemplate>
-      <GridContainer>
-        <CurrenciesList />
-      </GridContainer>
-    </CommonCurrenciesTemplate>
+    <CommonTemplate>
+      <Container>
+        <GridContainer>
+          <CurrenciesList />
+        </GridContainer>
+      </Container>
+    </CommonTemplate>
   )
 }
 
@@ -18,7 +21,12 @@ const GridContainer = styled.div`
   grid-template-columns: repeat(6, 1fr);
   grid-auto-rows: 256px;
   grid-gap: 10px;
-  @media (max-width: 1280px) {
+  @media (max-width: 1340px) {
+    grid-template-columns: repeat(4, 1fr);
+    grid-auto-rows: 200px;
+  }
+  @media (max-width: 870px) {
     grid-template-columns: repeat(3, 1fr);
+    grid-auto-rows: 200px;
   }
 `
