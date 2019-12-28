9716b5d9fd507fb61e431fd170778250d6cd70fa
Max Safin <a4ron5@yandex.ru>
2019-12-28 12:29:05 +0500

    ui(templates/common-template): removed container

--- src/ui/templates/common-template.js
+++ src/ui/templates/common-template.js
@@ -1,15 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { Header } from "./header"
-import { Container } from "./container"
 import { Footer } from "./footer"
 
 export const CommonTemplate = ({ children, header, footer }) => (
-  <Container>
+  <>
     {header && <Header>{header}</Header>}
     {children}
     {footer && <Footer>{footer}</Footer>}
-  </Container>
+  </>
 )
 
 CommonTemplate.propTypes = {
