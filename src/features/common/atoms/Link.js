4c8a410e49369687cdead2e108db87dd6d0e0615
Max Safin <a4ron5@yandex.ru>
2019-12-29 17:47:09 +0500

    features(common/atoms): added link, fix button

--- src/features/common/atoms/Link.js
+++ src/features/common/atoms/Link.js
@@ -1,6 +1,10 @@
 import styled from "styled-components"
+import { NavLink } from "react-router-dom"
 
-export const Button = styled.span`
+export const Link = styled(NavLink).attrs({
+  activeStyle: { opacity: "0.7" },
+})`
+  text-decoration: none;
   color: var(--primary-text);
   font-weight: bold;
   text-transform: uppercase;
@@ -10,5 +14,11 @@ export const Button = styled.span`
   cursor: pointer;
   &:hover {
     opacity: 0.7;
+  }
+  &:first-of-type {
+    margin-left: 0;
+  }
+  &:active {
+    color: red;
   }
 `
