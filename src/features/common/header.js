d5d7d12903cda74fe863b11403b94c65a876a950
Max Safin <a4ron5@yandex.ru>
2019-12-29 17:47:33 +0500

    features(common/header): fixed links

--- src/features/common/header.js
+++ src/features/common/header.js
@@ -3,8 +3,7 @@ import styled from "styled-components"
 import { useStore } from "effector-react"
 import { ThemeToggler } from "@features/themetoggler"
 import { Container } from "@ui/templates"
-import { history } from "@lib/routing"
-import { Button } from "./atoms/button"
+import { Button, Link } from "./atoms"
 import { $isAuth, checkedAuth } from "./model"
 
 export const Header = () => {
@@ -12,16 +11,12 @@ export const Header = () => {
 
   const handleClick = () => checkedAuth()
 
-  const toMain = () => history.push("/currencies")
-  const toFeatured = () => history.push("/featured")
-  const toAbout = () => history.push("/about")
-
   return (
     <Container>
       <Wrapper>
-        <Button onClick={toMain}>Main</Button>
-        <Button onClick={toFeatured}>Featured</Button>
-        <Button onClick={toAbout}>About</Button>
+        <Link to="/currencies">Main</Link>
+        <Link to="/featured">Featured</Link>
+        <Link to="/about">About</Link>
         <ThemeToggler />
         <Button onClick={handleClick}>{auth ? "Logout" : "Login"}</Button>
       </Wrapper>
