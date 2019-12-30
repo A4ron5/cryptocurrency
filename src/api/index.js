2bdd53cdb300dac74dbc8ddc0d8d4b478204911a
Max Safin <a4ron5@yandex.ru>
2019-12-30 14:38:22 +0500

    api: url fix

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,9 @@
 import axios from "axios"
 
 const API_KEY = "fd293c0ad2faf709dc61bf5a98faa0ae"
-const URL = "https://api.nomics.com/v1/currencies/ticker"
+const URL = `https://api.nomics.com/v1/currencies/ticker?key=${API_KEY}`
 
 export const request = async () => {
-  const data = await axios.get(`${URL}?key=${API_KEY}`)
+  const data = await axios.get(URL)
   return data
 }
