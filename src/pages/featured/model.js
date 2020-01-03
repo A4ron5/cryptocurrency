69f6826985ec3e4d2eba1c98aa2b5fc8d406d3c2
Max Safin <a4ron5@yandex.ru>
2020-01-03 12:04:35 +0500

    pages(featured/model): replaced sample with combine

--- src/pages/featured/model.js
+++ src/pages/featured/model.js
@@ -1,7 +1,10 @@
-import { sample } from "effector"
+import { combine } from "effector"
 import { $currencies } from "@features/currencies/model"
 import { $featured } from "@lib/heart"
 
-export const $sampled = sample($currencies, $featured, (currencies, featured) =>
-  currencies.filter((item) => featured.includes(item.id)),
+export const $combined = combine(
+  $currencies,
+  $featured,
+  (currnecies, featured) =>
+    currnecies.filter((item) => featured.includes(item.id)),
 )
