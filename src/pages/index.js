d1615099f9c9d1d5dadb68aa7ab6b6ebb181e441
Max Safin <a4ron5@yandex.ru>
2020-01-05 15:02:36 +0500

    pages: home/currency page added

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,18 @@ import { HomePage } from "./home/page"
 import { NotFoundPage } from "./notfound/page"
 import { AboutPage } from "./about/page"
 import { FeaturedPage } from "./featured/page"
+import { CurrencyPage } from "./home/currency/page"
 
 export const routes = () => [
   {
     path: "/currencies",
     exact: true,
     component: HomePage,
+  },
+  {
+    path: "/currencies/currency/:id",
+    exact: true,
+    component: CurrencyPage,
   },
   {
     path: "/about",
