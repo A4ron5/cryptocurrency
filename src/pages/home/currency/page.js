71390e6a8ed32c6296156c0b8da1a2c2a850f7a7
Max Safin <a4ron5@yandex.ru>
2020-01-05 15:02:07 +0500

    pages(home/currency): added

---
+++ src/pages/home/currency/page.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { CommonTemplate } from "@features/common"
+import { Container } from "@ui/templates"
+
+export const CurrencyPage = () => (
+  <CommonTemplate>
+    <Container>hello</Container>
+  </CommonTemplate>
+)
