b11b8f76f61367f4e55d332cfdae966579e672b4
Max Safin <a4ron5@yandex.ru>
2020-01-06 16:20:58 +0500

    api: added requestinfo

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,12 @@
-const URL = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`
-
 export const request = async () => {
+  const URL = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`
+  const res = await fetch(URL)
+  const data = await res.json()
+  return data
+}
+
+export const requestInfo = async (currency) => {
+  const URL = `https://api.coingecko.com/api/v3/coins/${currency.toLowerCase()}`
   const res = await fetch(URL)
   const data = await res.json()
   return data
